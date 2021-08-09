2a66005003d71cde58ea25e7b3c409d333bb1ec6
Sanjeet <singhsanjeetkumar11@gmail.com>
2021-08-09 17:13:15 +0530

    initial commit

---
+++ backend/src/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const DB = process.env.DATABASE;
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
---
+++ frontend/src/pages/single/Single.jsx
@@ -0,0 +1,11 @@
+import "./single.css";
+import SinglePost from "../../components/singlePost/SinglePost";
+
+const Single = () => {
+  return (
+    <div className="single">
+      <SinglePost/>
+    </div>
+  );
+}
+export default Single;
